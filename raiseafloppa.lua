dc7bdb3608b00da68edc458174ceb81531b16701
yuuiz <104612556+yuuiz@users.noreply.github.com>
2022-05-23 13:55:04 +0700

    ewvrewvfew

--- raiseafloppa.lua
+++
@@ -1,5 +0,0 @@
-for _,v in pairs(game:GetDescendants()) do
-	if v.Parent:IsA("Tool") and v.Parent.Name == "Meteorite" and v:IsA("BasePart") then
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-	end
-end
