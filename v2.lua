061dba0d996bbb67d4bbf63d2406abc83451ad40
yuuiz <104612556+yuuiz@users.noreply.github.com>
2023-01-17 23:55:23 -0800

    recode for new update

--- v2.lua
+++ v2.lua
@@ -20,23 +20,12 @@ local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local Window = Library:CreateWindow({
-    -- Set Center to true if you want the menu to appear in the center
-    -- Set AutoShow to true if you want the menu to appear when it is created
-    -- Position and Size are also valid options here
-    -- but you do not need to define them unless you are changing them :)
-
     Title = 'yuuix [BETA]',
     Center = true, 
     AutoShow = true,
 })
 local Main = Window:AddTab('Main')
 TabBox = Main:AddLeftGroupbox('OP')
-
-TabBox:AddToggle('infcash', {
-    Text = 'inf points',
-    Default = false, -- Default value (true / false)
-    Tooltip = 'Self-Explaintory but it will not be visible to anyone but you!', -- Information shown when you hover over the toggle
-})
 TabBox:AddToggle('noac', {
     Text = "anticheat bypass",
     Default = true,
@@ -52,21 +41,11 @@ TabBox:AddToggle('alwayrun', {
     Default = false,
     Tooltip = "Self-Explaintory, prevent BOOM making you can't run",
 })
-TabBox:AddToggle('fakesurvive', {
-    Text = 'Fake-Survive',
-    Default = false,
-    Tooltip = "This will make the server thinks your still alive, and you will still get items and point from hour even if you died.",
-})
 TabBox:AddToggle('loopkill', {
     Text = 'Loop kill rake',
     Default = false,
     Tooltip = "This will make rake ded forever and dont grant points so kid will be mad",
 })
-TabBox:AddButton('kill rake', function()
-    pcall(function()
-        DamageRake(9999999999)
-    end)
-end)
 TabBox:AddButton('get tools', function()
     pcall(function()
         local folder : Folder = plr.PlayerGui.ShopGui.ShopTools
@@ -79,22 +58,6 @@ TabBox:AddButton('get tools', function()
 		end
 	end)
 end)
-TabBox:AddButton('heal rake', function()
-    pcall(function()
-        DamageRake(-9999999999)
-
-    end)
-end)
-TabBox:AddButton('get totems', function()
-    pcall(function()
-        local totems : Folder = game:GetService("Workspace").BuffNoobRequirement.Noobs
-        for _,v in pairs(totems:GetDescendants()) do
-            if v:IsA("BasePart") then
-                plr.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
-            end
-        end
-    end)
-end)
 TabBox:AddButton('Unload', function() Library:Unload() end)
 TabBox2 = Main:AddRightGroupbox('Player')
 TabBox2:AddToggle('speed', {
@@ -105,18 +68,16 @@ TabBox2:AddToggle('speed', {
 
 TabBox2:AddInput('walkspeed', {
     Default = '10',
-    Numeric = true, -- true / false, only allows numbers
-    Finished = true, -- true / false, only calls callback when you press enter
+    Numeric = true, 
+    Finished = true, 
 
     Text = 'Walkspeed',
-    Tooltip = 'you go zoom', -- Information shown when you hover over the textbox
+    Tooltip = 'you go zoom', 
 
-    Placeholder = 'Type a number here!', -- placeholder text when the box is empty
-    -- MaxLength is also an option which is the max length of the text
+    Placeholder = 'Type a number here!',
 })
 Options.walkspeed:OnChanged(function()
-    if plr.Character then
-        if Toggles.speed.Value == false then return end
+    if plr.Character and Toggles.speed.Value == true then
         local walkspeed : NumberValue = plr.Character.StaminaValues.WalkSpeed
         --local runspeed : NumberValue = plr.Character.StaminaValues.RunSpeed
         walkspeed.Value = Options.walkspeed.Value
@@ -125,18 +86,16 @@ Options.walkspeed:OnChanged(function()
 end)
 TabBox2:AddInput('runspeed', {
     Default = '35',
-    Numeric = true, -- true / false, only allows numbers
-    Finished = true, -- true / false, only calls callback when you press enter
+    Numeric = true,
+    Finished = true,
 
     Text = 'Runspeed',
-    Tooltip = 'you go zoom', -- Information shown when you hover over the textbox
+    Tooltip = 'you go zoom',
 
-    Placeholder = 'Type a number here!', -- placeholder text when the box is empty
-    -- MaxLength is also an option which is the max length of the text
+    Placeholder = 'Type a number here!',
 })
 Options.runspeed:OnChanged(function()
-    if plr.Character then
-        if Toggles.speed.Value == false then return end
+    if plr.Character and Toggles.speed.Value = true then
         --local walkspeed : NumberValue = plr.Character.StaminaValues.WalkSpeed
         local runspeed : NumberValue = plr.Character.StaminaValues.RunSpeed
         --walkspeed.Value = Options.walkspeed.Value
@@ -152,12 +111,9 @@ TabBox2:AddInput('jumppower', {
     Tooltip = 'you go weee', -- Information shown when you hover over the textbox
 
     Placeholder = 'Type a number here!', -- placeholder text when the box is empty
-    -- Max
-    --Length is also an option which is the max length of the text
 })
 Options.jumppower:OnChanged(function()
-    if plr.Character:FindFirstChildOfClass("Humanoid") then
-        if Toggles.speed.Value == false then return end
+    if plr.Character:FindFirstChildOfClass("Humanoid")  and Toggles.speed.Value == true then
         if Options.jumppower.Value == 0 then
             plr.Character:FindFirstChildOfClass("Humanoid").JumpPower = 50
         end
@@ -185,30 +141,23 @@ while task.wait() do
         break
     end
     game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, true)
-    if Toggles.infcash.Value == true then
-        pcall(function() plr.leaderstats.Points.Value = 9e10 end)
-    end
     if Toggles.noac.Value == true then
-        pcall(function()
-            local fixstuff : LocalScript = plr.Character.FixStuff
-            local mainloader : LocalScript = plr.Character.MainLoader
-            local antiexploit : Script = plr.Character.ANTIEXPLOIT
+        if plr.Character and plr.Character:FindFirstChild('ANTIEXPLOIT') then
+            local antiexploit = plr.Character:FindFirstChild('ANTIEXPLOIT')
 
-            fixstuff:Destroy()
-            mainloader:Destroy()
-            antiexploit:Destroy()
-        end)
+            if antiexploit then
+				antiexploit:Destroy()
+			end
+        end
     end
     if Toggles.infrun.Value == true then
-        pcall(function()    plr.Character.StaminaValues.Stamina.Value = 200 end)
+        if plr.Character and plr.Character:FindFirstChild('StaminaValues') and plr.Character.StaminaValues:FindFirstChild('Stamina') then
+			plr.Character.StaminaValues.Stamina.Value = 200
+		end
     end
     if Toggles.alwayrun.Value == true then
-        pcall(function() plr.Character.StaminaValues.CanRun.Value = true end)
-    end
-    if Toggles.fakesurvive.Value == true then
-        pcall(function() plr.Character.SurvivedClient.Disabled = true end)
-    end
-    if Toggles.loopkill.Value == true then
-        DamageRake(9999999999)
+        if plr.Character and plr.Character:FindFirstChild('StaminaValues') and plr.Character:FindFirstChild('CanRun') then
+			plr.Character.StaminaValues.CanRun.Value = true
+		end
     end
 end
